a93903f6128acca76b56f93371de798dfd90e5c0
George Kaluwin <georgekaluwin@gmail.com>
2022-08-11 17:16:02 +0930

    server.js changes

---
+++ server.js
@@ -0,0 +1,76 @@
+// dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { json } = require("express");
+const { get } = require("http");
+const util = erquire("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writefile);
+
+// server set up
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// GET request
+app.get("api/notes", function(req, res) {
+    readFileAsync("./db/db.json", "utf8").then(function(data){
+      notes=[].concat(JSON.parse(data))
+      res.json(notes);
+    })
+});
+
+// API route | POST request
+app.post("/api/notes", function(req, res) {
+    const note = req.body;
+    readFileAsync("./db/db.json", "utf8").then(function(data){
+      const notes = [].concat(JSON.parse(data));
+      note.id = notes.length + 1
+      notes.push(note);
+      return notes
+    }).then(function(notes) {
+      writeFileAsync("./db/db.json", JSON.stringify(notes))
+      res.json(note);
+    })
+  });
+
+  // API Route | DELETE request
+  appp.delete("./api/notes/:id", function(req,res){
+    const idToDelete = ParseInt(req.params.id);
+    readFileAsync("./db/db.json", "utf8").then(function(data){
+      const notes = [].concat(JSON.parse(data));
+      const newNotesData = []
+      for (let i = 0; i<notes.length; i++) {
+        if(idToDelete !== notes[i].id){
+          newNotesData.push(notes[i])
+        }
+      }
+      return newNotesData
+    }).then(function(notes){
+      writeFileAsync("./db/db.json", JSON.stringify(notes))
+      res.send('saved succesfully!');
+    })
+  });
+
+// HTML routes
+app.get("/notes", function(req,res){
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/", function(req,res){
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("*", function(req,res){
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// Listen
+app.listen(PORT, function(){
+  console.log("App listening on PORT " + PORT)
+});
